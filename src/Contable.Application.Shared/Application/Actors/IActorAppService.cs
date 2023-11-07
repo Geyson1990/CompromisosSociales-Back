f576b878604269d62ae99dc5eb60f6a31e476516
hhuamanchumoa <68071094+hhuamanchumoa@users.noreply.github.com>
2023-11-07 11:50:05 -0500

    Update IActorAppService.cs

--- src/Contable.Application.Shared/Application/Actors/IActorAppService.cs
+++ src/Contable.Application.Shared/Application/Actors/IActorAppService.cs
@@ -10,10 +10,10 @@ namespace Contable.Application.Actors
 {
     public interface IActorAppService : IApplicationService
     {
-        Task Create(ActorCreateDto input);
+        Task<EntityDto<long>> Create(ActorCreateDto input);
         Task Delete(EntityDto input);
         Task<ActorGetDataDto> Get(NullableIdDto input);
         Task<PagedResultDto<ActorGetAllDto>> GetAll(ActorGetAllInputDto input);
-        Task Update(ActorUpdateDto input);
+        Task<EntityDto> Update(ActorUpdateDto input);
     }
 }
