28d9217d3d7d998b6b8fdfd0a2565685bdad2376
hhuamanchumoa <68071094+hhuamanchumoa@users.noreply.github.com>
2023-11-28 09:35:39 -0500

    Update ActorGetAllDto.cs
    Add column StatusId

--- src/Contable.Application.Shared/Application/Actors/Dto/ActorGetAllDto.cs
+++ src/Contable.Application.Shared/Application/Actors/Dto/ActorGetAllDto.cs
@@ -19,6 +19,7 @@ namespace Contable.Application.Actors.Dto
         public bool IsPoliticalAssociation { get; set; }
         public string PoliticalAssociation { get; set; }
         public bool Enabled { get; set; }
+        public int StatusId { get; set; }
         public ActorTypeDto ActorType { get; set; }
         public ActorMovementDto ActorMovement { get; set; }   
         //public int TypologyId { get; set; }
